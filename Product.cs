1ee557640262be5c4c92c04c68a946364dba78ac
HFarhad2843 <146637915+HFarhad2843@users.noreply.github.com>
2024-01-09 01:17:19 +0400

    first commit

---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp15
+{
+    public class Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp15;
+
+Console.WriteLine("System IO:");
+
+//Directory.CreateDirectory(@".\Admin");
+Product p1 = new(3, "Motorolla", 470);
+if (!File.Exists(@".\Admin\admin.txt"))
+{
+    File.Create(@".\Admin\admin.txt");
+}
+
+StreamReader sr = new StreamReader(@".\Admin\admin.txt");
+while (!sr.EndOfStream)
+{
+    Console.WriteLine(sr.ReadLine());
+}
+sr.Close();
+
+StreamWriter sw = new StreamWriter(@".\Admin\admin.txt", true);
+sw.WriteLine($"Id:{p1.Id}; Name : {p1.Name}; Price: {p1.Price}");
+sw.Close();
